bc98f4cb5c258abd1f418cd79483358f933efd11
Trang Truong <trangldtruong@gmail.com>
2023-10-10 17:11:04 -0400

    some errors occur, initial js redone

---
+++ assets/script.js
@@ -0,0 +1,28 @@
+var buttonEl = document.querySelector('#search-button')
+var apiKey = "fdc35111e213fb02c228dce2699bda6b"
+var userInput = document.querySelector('#user-input')
+var city = userInput.textContent
+var formSubmitHandler = function (event) {
+    event.preventDefault();
+}
+//function to find latitude and longitude 
+var getCoord = function (city) {
+fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit={limit}&appid=${apiKey}`)
+  .then(function (response) {
+    if (response.ok) {
+        console.log(response)
+        response.json().then(function (data) {
+            console.log(data);
+
+        })
+    }
+})};
+
+var renderCoord = buttonEl.addEventListener('click', getCoord)
+//function to find current weather 
+
+//function to find forecast
+
+//function to display current weather 
+
+//function to display forecast 
