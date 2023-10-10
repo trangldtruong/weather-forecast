3db37e35e2814c356b61cda9aa3d02b394b8e1e7
Trang Truong <trangldtruong@gmail.com>
2023-10-10 19:36:42 -0400

    readme.md added

--- README.md
+++ README.md
@@ -1,11 +1,16 @@
 # WEATHER FORECAST  
 
 ## Description
+This application aims to provide information on weather of a certain city for the current day and the next five days
 
 ## Usage 
+Users can enter city name at input and click search button.
 
 ## Visual
-
+A screenshot of the application can be found below 
+<img>
 ## License
+MIT
 
 ## Contribution 
+The author is open to any improvement or contribution to the codebase. Please clone the project at and create a pull request when improvement is added
--- assets/script.js
+++ assets/script.js
@@ -2,6 +2,8 @@ var buttonEl = document.querySelector('#search-button')
 var apiKey = "fdc35111e213fb02c228dce2699bda6b"
 var userInput = document.querySelector('#user-input')
 var city = userInput.textContent
+var lat = ""
+var lon = ""
 var formSubmitHandler = function (event) {
     event.preventDefault();
 }
@@ -18,11 +20,20 @@ fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${city}&appid=${apiKey}`)
     }
 })};
 
-var renderCoord = buttonEl.addEventListener('click', getCoord)
 //function to find current weather 
+fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}`) 
+   .then(function (response) {
+    if (response.ok) {
+        console.log(response)
+        response.json().then(function (data) {
+            console.log(data)
+        })
+    }
+})
 
 //function to find forecast
-
+fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}`)
 //function to display current weather 
-
+var currentTemperature = document.querySelector('#current-temperature')
 //function to display forecast 
+var renderCoord = buttonEl.addEventListener('click', getCoord)
