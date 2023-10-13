a2d2af6c49fd0518168ea836398011b707d99ca1
Trang Truong <trangldtruong@gmail.com>
2023-10-12 22:32:40 -0400

    update function

--- assets/script.js
+++ assets/script.js
@@ -21,7 +21,7 @@ fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${city}&appid=${apiKey}`)
 })};
 
 function currentWeather(lat, lon, city){
-//function to find current weather 
+//function to find and display current weather 
 fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=imperial&appid=${apiKey}`) 
    .then(function (response) {
     if (response.ok) {
@@ -37,7 +37,7 @@ fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&uni
             humidity.text("Humidity: "+data.main.humidity);
 
             let wind = $("<p>");
-            wind.text("Wind Speed: "+data.wind.speed);
+            wind.text("Wind: "+data.wind.speed);
 
             $("#current-weather").append(h2, temp, humidity, wind)
         })
@@ -45,7 +45,7 @@ fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&uni
 })
 }
 
-//function to find forecast
+//function to find and display 5-day forecast
 function forecastWeather(lat, lon){
 fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=imperial&appid=${apiKey}`)
 .then(function(res){
@@ -54,18 +54,27 @@ fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&un
 .then(function(forecastData){
     for(var i=0; i<forecastData.list.length;i++){
         if(forecastData.list[i].dt_txt.split(" ")[1] === "03:00:00"){
-            console.log("test");
+            console.log(forecastData);
             //create a div and assign class "col-md-2" and then append it to the #five-day-forecast div
             // create <p> for temp, humidity and wind
             //append them to col-md-2 div
+            let temp = $("<p>");
+            temp.text("Temp: "+data.main.temp);
+
+            let humidity = $("<p>");
+            humidity.text("Humidity: "+data.main.humidity);
+
+            let wind = $("<p>");
+            wind.text("Wind: "+data.wind.speed);
+
+            $("#five-day-forecast").append(h2, temp, humidity, wind)
         }
     }
 })
-//function to display current weather 
-var currentTemperature = document.querySelector('#current-temperature')
+
 //function to clear current weather 
 }
-//function to display forecast 
+//event listener
 var renderCoord = buttonEl.addEventListener('click', function(){
     var city = document.getElementById("user-input").value;
     getCoord(city);
